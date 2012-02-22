e3bad222f9e2f7c12660ae0de58736531917d0ff
dkln <dkln@altovista.nl>
2012-02-22 09:29:27 +0100

    newest solr version + we dont need a checksum

--- solr/attributes/default.rb
+++ solr/attributes/default.rb
@@ -2,9 +2,8 @@ include_attribute "jetty"
 
 expand!
 
-default[:solr][:version]   = "1.4.1"
+default[:solr][:version]   = "3.5.0"
 default[:solr][:link]      = "http://apache.favoritelinks.net/lucene/solr/#{solr.version}/apache-solr-#{solr.version}.tgz"
-default[:solr][:checksum]  = "d795bc477335b3e29bab7073b385c93fca4be867aae345203da0d1e438d7543f"
 default[:solr][:directory] = "/usr/local/src"
 default[:solr][:download]  = "#{solr.directory}/apache-solr-#{solr.version}.tgz"
 default[:solr][:extracted] = "#{solr.directory}/apache-solr-#{solr.version}"
